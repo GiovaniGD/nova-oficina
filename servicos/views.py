c16d663440da469e90bc8cd45665f16469a3cb86
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-12-04 13:53:40 -0300

    tudo pronto

--- servicos/views.py
+++ servicos/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse, FileResponse
 from .models import Servico
 from fpdf import FPDF
 from io import BytesIO
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url="/usuarios/login/")
 def novo_servico(request):
     if request.method == "GET":
         form = FormServico()
@@ -16,12 +18,14 @@ def novo_servico(request):
             return redirect('/servicos/listar_servico/')
         else:
             return render(request, 'novo_servico.html', {'form': form})
-        
+
+@login_required(login_url="/usuarios/login/")
 def listar_servico(request):
     if request.method == "GET":
         servicos = Servico.objects.all()
         return render(request, 'listar_servico.html', {'servicos': servicos})
     
+@login_required(login_url="/usuarios/login/")
 def servico(request, servico_id):
     servico = Servico.objects.get(id=servico_id)
     categorias = servico.categoria_manutencao.all()
